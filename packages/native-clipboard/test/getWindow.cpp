01b22058a797befa1c7669e60b6971cbc108b4a2
showlotus <showlotus2000@gmail.com>
2024-08-10 23:22:29 +0800

    **

--- packages/native-clipboard/test/getWindow.cpp
+++ packages/native-clipboard/test/getWindow.cpp
@@ -26,17 +26,61 @@ HWND GetHwndByPid(DWORD dwProcessID) {
     return retHwnd;
 }
 
+// 模拟 Ctrl + V 事件触发粘贴
+void simulatePaste() {
+    keybd_event(VK_CONTROL, 0, 0, 0);
+    keybd_event('V', 0, 0, 0);
+    keybd_event('V', 0, KEYEVENTF_KEYUP, 0);
+    keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+}
+
+bool IsForegroundWindowFocused() {
+    // 获取当前前台窗口的句柄
+    HWND foregroundWindow = GetForegroundWindow();
+    if (foregroundWindow == NULL) {
+        std::cout << "无法获取前台窗口的句柄。" << std::endl;
+        return false;
+    }
+
+    // 获取当前键盘焦点的窗口句柄
+    HWND focusedWindow = GetFocus();
+    if (focusedWindow == NULL) {
+        // 键盘焦点不在当前线程的任何窗口上
+        std::cout << "没有窗口有键盘焦点。" << std::endl;
+        return false;
+    }
+
+    // 比较前台窗口和焦点窗口的句柄
+    return (foregroundWindow == focusedWindow);
+}
+
 int main() {
-    HWND hwnd = GetHwndByPid(124200);
+    HWND hwnd = GetHwndByPid(12680);
+    // HWND hwnd = GetHwndByPid(9224);
     // hwnd = GetForegroundWindow();
     SetForegroundWindow(hwnd);
-    Sleep(1000);
-    if (OpenClipboard(NULL)) {
-        if (IsClipboardFormatAvailable(CF_TEXT)) {
-            std::cout << "pasted" << std::endl;
-            SendMessage(hwnd, WM_PASTE, 0, 0);
-        }
+    // Sleep(1000);
+    // simulatePaste();
+    
+    // if (OpenClipboard(NULL)) {
+    //     HANDLE hData = GetClipboardData(CF_TEXT);
+    //     if (hData != NULL) {
+    //         char* pszText = static_cast<char*>(GlobalLock(hData));
+    //         if (pszText != NULL) {
+    //             std::cout << "剪贴板内容: " << pszText << std::endl;
+    //             GlobalUnlock(hData);
+    //         }
+    //     }
+    //     CloseClipboard();
+    // }
+    // Sleep(500);
+    if (GetFocus() == hwnd) {
+        std::cout << "has focus" << std::endl;
     }
-    // BringWindowToTop(hwnd);
+    if (OpenClipboard(NULL) && IsClipboardFormatAvailable(CF_TEXT)) {
+        std::cout << "pasted" << std::endl;
+        simulatePaste();
+        // SendMessage(hwnd, WM_PASTE, 0, 0);
+    }
     return 0;
 }
