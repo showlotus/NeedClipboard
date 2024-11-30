65c3b976da9e066154de2af1b8062f387548eb8a
showlotus <2257030399@qq.com>
2024-11-30 21:04:28 +0800

    feat: add log

--- electron/main/updater.ts
+++ electron/main/updater.ts
@@ -1,14 +1,22 @@
 import { app, dialog } from 'electron'
+import log from 'electron-log/main'
 import { autoUpdater } from 'electron-updater'
 
 autoUpdater.autoDownload = false
 autoUpdater.autoInstallOnAppQuit = true
 
-autoUpdater.on('checking-for-update', () => {})
-autoUpdater.on('update-available', () => {
-  autoUpdater.downloadUpdate()
+log.initialize()
+log.transports.console.format = '[{h}:{i}:{s}] {text}'
+
+autoUpdater.on('checking-for-update', () => {
+  log.info('checking for update')
 })
-autoUpdater.on('update-not-available', () => {})
+autoUpdater.on('update-available', async () => {
+  log.info('download process:', await autoUpdater.downloadUpdate())
+})
+autoUpdater.on('update-not-available', () => {
+  log.info('not update available')
+})
 autoUpdater.on('update-downloaded', () => {
   dialog
     .showMessageBox({
@@ -19,12 +27,15 @@ autoUpdater.on('update-downloaded', () => {
     })
     .then((res) => {
       if (res.response === 0) {
+        log.info('quit and install')
         autoUpdater.quitAndInstall()
         app.quit()
       }
     })
 })
-autoUpdater.on('error', () => {})
+autoUpdater.on('error', (e) => {
+  log.error('update error', e)
+})
 
 export function checkUpdate() {
   autoUpdater.checkForUpdates()
